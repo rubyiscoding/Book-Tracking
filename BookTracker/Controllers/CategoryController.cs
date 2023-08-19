1c7a05676d07a612f3fb85de09290c31c8403498
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-08-18 23:52:19 -0400

    Code cleanup.

--- BookTracker/Controllers/CategoryController.cs
+++ BookTracker/Controllers/CategoryController.cs
@@ -83,7 +83,6 @@ namespace BookTracker.Controllers
                 return RedirectToAction(nameof(Index));
             }
         }
-
         // POST: Category/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
